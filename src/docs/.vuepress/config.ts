85f48f691869c0091b85fd2a38881eac9e283a52
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-23 16:51:02 +0600

    Add Navigation

--- src/docs/.vuepress/config.ts
+++ src/docs/.vuepress/config.ts
@@ -1,7 +1,30 @@
 import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'Alexander Radchenko GitHub site',
   description: 'Just playing around',
+
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: 'Любимой 💕',
+        link: '/ForTheBeloved/',
+        children: [
+          '/ForTheBeloved/',
+          '/ForTheBeloved/HappyNewYear.md'
+        ],
+      }
+    ],
+    sidebar: [
+      {
+        text: 'Привет 👋, Любимая 💕',
+        link: '/ForTheBeloved/',
+        children: [
+          '/ForTheBeloved/HappyNewYear.md'
+        ]
+      }
+    ]
+  }),
 })
