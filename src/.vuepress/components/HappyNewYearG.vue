4c810205c0ab3892376d096ba32d780cda46956b
Alexander Radchenko <radchenkosasha@gmail.com>
2022-12-26 17:08:27 +0600

    Add Transitions

--- src/.vuepress/components/HappyNewYearG.vue
+++ src/.vuepress/components/HappyNewYearG.vue
@@ -1,64 +1,85 @@
 <template>
   <div ref="scene" style="position: relative;
       box-sizing: border-box; margin: 0px; width: calc(100% * 1.2); padding: calc(33% * 1.2);
-      overflow: hidden;
-      background-color: lightblue;"
+      overflow: hidden; "
+      :class="{ night : BellOfHappiness_playState,  day : !BellOfHappiness_playState, }"
   >
-    <Vue3Lottie ref="HeartFlyPartical" :animationData="HeartFlyPartical_Json" style="position: absolute;
-        width: 100%; left: 0%; top: 0%; "
-      :speed="0.2"
-      v-show="!BellOfHappiness_playState"
-    />
-    <!-- <Vue3Lottie ref="Snow" :animationData="Snow_Json" style="position: absolute;
-        width: 125%; left: 0%; top: 0%; "
-      v-show="BellOfHappiness_playState"
-    /> -->
-    <Vue3Lottie ref="SnowFlakes" :animationData="SnowFlakes_Json" style="position: absolute;
-        width: 125%; left: 0%; top: 0%; "
-      v-show="BellOfHappiness_playState"
-    />
+    <Transition>
+      <Vue3Lottie ref="HeartFlyPartical" :animationData="HeartFlyPartical_Json" style="position: absolute;
+          width: 100%; left: 0%; top: 0%; "
+        :speed="0.2"
+        v-show="!BellOfHappiness_playState"
+      />
+    </Transition>
+    <!-- <Transition>
+      <Vue3Lottie ref="Snow" :animationData="Snow_Json" style="position: absolute;
+          width: 125%; left: 0%; top: 0%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition> -->
+    <Transition>
+      <Vue3Lottie ref="SnowFlakes" :animationData="SnowFlakes_Json" style="position: absolute;
+          width: 125%; left: 0%; top: 0%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition>
     <Vue3Lottie ref="FirePlace" :animationData="FirePlace_Json" style="position: absolute;
         width: 40%; left: -3%; top: -10%; "
     />
     <Vue3Lottie ref="SantaSleigh" :animationData="SantaSleigh_Json" style="position: absolute;
         width: 200%; left: 0%; top: -55%; "
+        :speed="0.5"
     />
-    <Vue3Lottie ref="RocketLaunches" :animationData="RocketLaunches_Json" style="position: absolute;
-        width: 50%; right: -14%; top: 0%; "
-      v-show="BellOfHappiness_playState"
-    />
-    <Vue3Lottie ref="WebDev" :animationData="WebDev_Json" style="position: absolute;
-        width: 50%; left: 0%; bottom: 0%; "
-      direction="alternate"
-      v-show="!BellOfHappiness_playState"
-    />
-    <Vue3Lottie ref="DrawingWoman" :animationData="DrawingWoman_Json" style="position: absolute;
-        width: 40%; left: 40%; top: 0%; "
-      v-show="!BellOfHappiness_playState"
-    />
+    <Transition>
+      <Vue3Lottie ref="RocketLaunches" :animationData="RocketLaunches_Json" style="position: absolute;
+          width: 50%; right: -14%; top: 0%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition>
+    <Transition>
+      <Vue3Lottie ref="WebDev" :animationData="WebDev_Json" style="position: absolute;
+          width: 50%; left: 0%; bottom: 0%; "
+        direction="alternate"
+        v-show="!BellOfHappiness_playState"
+      />
+    </Transition>
+    <Transition>
+      <Vue3Lottie ref="DrawingWoman" :animationData="DrawingWoman_Json" style="position: absolute;
+          width: 40%; left: 40%; top: 0%; "
+        v-show="!BellOfHappiness_playState"
+      />
+    </Transition>
     <Vue3Lottie ref="ChristmasTree" :animationData="ChristmasTree_Json" style="position: absolute;
         width: 60%; right: -7%; bottom: -8%; "
       direction="alternate"
     />
-    <Vue3Lottie ref="CatLoader" :animationData="CatLoader_Json" style="position: absolute;
-        width: 20%; right: -3%; bottom: -6%; "
-      v-show="!BellOfHappiness_playState"
-    />
-    <Vue3Lottie ref="AnimationSuccessBack" :animationData="AnimationSuccessBack_Json" style="position: absolute;
-        width: 70%; left: 0%; bottom: -5%; "
-      v-show="BellOfHappiness_playState"
-    />
+    <Transition>
+      <Vue3Lottie ref="CatLoader" :animationData="CatLoader_Json" style="position: absolute;
+          width: 20%; right: -3%; bottom: -6%; "
+        v-show="!BellOfHappiness_playState"
+      />
+    </Transition>
+    <Transition>
+      <Vue3Lottie ref="AnimationSuccessBack" :animationData="AnimationSuccessBack_Json" style="position: absolute;
+          width: 70%; left: 0%; bottom: -5%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition>
     <Vue3Lottie ref="Lights" :animationData="Lights_Json" style="position: absolute;
         width: 50%; left: -2%; top: -30%; "
     />
-    <Vue3Lottie ref="MerryChristmas" :animationData="MerryChristmas_Json" style="position: absolute;
-        width: 60%; left: 40%; top: -30%; "
-      v-show="BellOfHappiness_playState"
-    />
-    <Vue3Lottie ref="RabbitRunning" :animationData="RabbitRunning_Json" style="position: absolute;
-        width: 30%; left: -6%; bottom: -10%; "
-      v-show="BellOfHappiness_playState"
-    />
+    <Transition>
+      <Vue3Lottie ref="MerryChristmas" :animationData="MerryChristmas_Json" style="position: absolute;
+          width: 60%; left: 40%; top: -30%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition>
+    <Transition>
+      <Vue3Lottie ref="RabbitRunning" :animationData="RabbitRunning_Json" style="position: absolute;
+          width: 30%; left: -6%; bottom: -10%; "
+        v-show="BellOfHappiness_playState"
+      />
+    </Transition>
     <Vue3Lottie ref="BellOfHappiness" :animationData="BellOfHappiness_Json" style="position: absolute;
         width: 20%; top: -5%; right: -3%; "
       :autoPlay="BellOfHappiness_playState"
@@ -67,6 +88,27 @@
     />
   </div>
 </template>
+
+<style>
+  .day {
+    background-color: lightblue;
+    transition: all 3s ease;
+  }  
+  .night {
+    background-color: darkorchid;
+    transition: all 3s linear;
+  }  
+
+  .v-enter-active,
+  .v-leave-active {
+    transition: opacity 3s ease;
+  }
+
+  .v-enter-from,
+  .v-leave-to {
+    opacity: 0;
+  }
+</style>
 
 <script>
 import { Vue3Lottie } from 'vue3-lottie'
@@ -88,8 +130,8 @@ import MerryChristmas_Json from './HappyNewYear/lotties/88762-merry-christmas.js
 import RabbitRunning_Json from './HappyNewYear/lotties/106450-rabbit-running.json'
 import BellOfHappiness_Json from './HappyNewYear/lotties/131384-bell-of-happiness.json'
 
-//import BingCrosby_JingleBells_mp3 from './HappyNewYear/sounds/Bing Crosby--Jingle Bells (1943).mp3'
-import DiscoCrash_NewYear_mp3 from './HappyNewYear/sounds/Дискотека Авария--Новогодняя.mp3'
+//import BingCrosby_JingleBells_mp3 from './HappyNewYear/sounds/Bing_Crosby--Jingle_Bells_(1943).mp3'
+import DiscoCrash_NewYear_mp3 from './HappyNewYear/sounds/Disco_Crash--New Year.mp3'
 
 var audio = new Audio(DiscoCrash_NewYear_mp3);
 audio.loop = true;
