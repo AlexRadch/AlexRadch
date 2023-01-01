d5d26382ef0bc0b1e65639ada9f93af07e5958a9
Alexander Radchenko <radchenkosasha@gmail.com>
2023-01-02 01:26:29 +0600

    Refactoring HappyNewYearG.vue to typescript and script setup

--- src/.vuepress/components/HappyNewYearG.vue
+++ src/.vuepress/components/HappyNewYearG.vue
@@ -1,7 +1,6 @@
 <template>
-  <div ref="scene" style="position: relative;
-      box-sizing: border-box; margin: 0px; width: calc(100% * 1.2); padding: calc(33% * 1.2);
-      overflow: hidden; "
+  <div style="position: relative; box-sizing: border-box; margin: 0px;
+      width: calc(100% * 1.2); padding: calc(33% * 1.2); overflow: hidden;"
     :class="{ [HappyNewYearG.night] : BellOfHappiness_playState,  [HappyNewYearG.day] : !BellOfHappiness_playState, }"
   >
     <Transition>
@@ -19,10 +18,10 @@
         :autoPlay="BellOfHappiness_playState"
       />
     </Transition>
-    <LottieAnimation ref="FirePlace" :animationData="FirePlace_Json" loop style="position: absolute;
+    <LottieAnimation :animationData="FirePlace_Json" loop style="position: absolute;
         width: 40%; left: -3%; top: -10%; "
     />
-    <LottieAnimation ref="SantaSleigh" :animationData="SantaSleigh_Json" loop style="position: absolute;
+    <LottieAnimation :animationData="SantaSleigh_Json" loop style="position: absolute;
         width: 200%; left: 0%; top: -55%; "
         :speed="0.5"
     />
@@ -30,7 +29,7 @@
       <LottieAnimation ref="RocketLaunches" :animationData="RocketLaunches_Json" loop style="position: absolute;
           width: 50%; right: -14%; top: 0%; "
         v-show="BellOfHappiness_playState"
-        :pauseAnimation="!BellOfHappiness_playState && DayEntered"
+        :autoPlay="BellOfHappiness_playState"
       />
     </Transition>
     <Transition>
@@ -44,8 +43,8 @@
     <Transition>
       <LottieAnimation ref="DrawingWoman" :animationData="DrawingWoman_Json" loop style="position: absolute;
           width: 40%; left: 40%; top: 0%; "
-        :autoPlay="!BellOfHappiness_playState"
         v-show="!BellOfHappiness_playState"
+        :autoPlay="!BellOfHappiness_playState"
       />
     </Transition>
     <LottieAnimation ref="ChristmasTree" :animationData="ChristmasTree_Json" loop style="position: absolute;
@@ -66,7 +65,7 @@
         :autoPlay="BellOfHappiness_playState"
       />
     </Transition>
-    <LottieAnimation ref="Lights" :animationData="Lights_Json" loop style="position: absolute;
+    <LottieAnimation :animationData="Lights_Json" loop style="position: absolute;
         width: 50%; left: -2%; top: -30%; "
     />
     <Transition>
@@ -76,7 +75,7 @@
         :autoPlay="BellOfHappiness_playState"
       />
     </Transition>
-    <Transition @before-enter="night_BeforeEnter" @after-enter="night_AfterEnter" @after-leave="night_AfterLeave">
+    <Transition @before-enter="night_BeforeEnter" @after-enter="night_AfterEnter" @before-leave="day_BeforeEnter" @after-leave="day_AfterLeave">
       <LottieAnimation ref="RabbitRunning" :animationData="RabbitRunning_Json" loop style="position: absolute;
           width: 30%; left: -6%; bottom: -10%; "
         :autoPlay="BellOfHappiness_playState"
@@ -88,12 +87,11 @@
       :autoPlay="BellOfHappiness_playState"
       @click="bellOfHappiness_OnClick"
     />    
+    <audio ref="Audio" hidden="true" loop><source src="@components/HappyNewYear/sounds/Disco_Crash--New Year.mp3" type="audio/mpeg"></audio>
   </div>
 </template>
 
-<script>
-// import { LottieAnimation } from 'lottie-web-vue'
-
+<script lang="ts">
 import HeartFlyPartical_Json from './HappyNewYear/lotties/43296-heart-fly-partical-transparent-bg-ver2.json'
 import SnowFlakes_Json from './HappyNewYear/lotties/130377-snowflakes.json'
 import FirePlace_Json from './HappyNewYear/lotties/129654-fire-place.json'
@@ -108,147 +106,120 @@ import Lights_Json from './HappyNewYear/lotties/129650-lights.json'
 import MerryChristmas_Json from './HappyNewYear/lotties/88762-merry-christmas.json'
 import RabbitRunning_Json from './HappyNewYear/lotties/106450-rabbit-running.json'
 import BellOfHappiness_Json from './HappyNewYear/lotties/131384-bell-of-happiness.json'
+</script>
 
-import DiscoCrash_NewYear_mp3 from './HappyNewYear/sounds/Disco_Crash--New Year.mp3'
+<script setup lang="ts">
+import { ref } from 'vue'
 
-var audio;
-var webDev_Direction = 1;
-var christmasTree_Direction = 1;
+const HeartFlyPartical = ref();
+const SnowFlakes = ref();
+const RocketLaunches = ref();
+const WebDev = ref();
+const DrawingWoman = ref();
+const ChristmasTree = ref();
+const CatLoader = ref();
+const AnimationSuccessBack = ref();
+const MerryChristmas = ref();
+const RabbitRunning = ref();
+const BellOfHappiness = ref();
 
-export default {
-  name: 'HappyNewYearG',
+const Audio = ref();
+const BellOfHappiness_playState = ref(false);
 
-  // components: {
-  //   //LottieAnimation,
-  // },
+function bellOfHappiness_OnClick() {
+  BellOfHappiness_playState.value = !BellOfHappiness_playState.value;
+  stateChanged();
+}
 
-  data() {
-    return {
-      HeartFlyPartical_Json,
-      SnowFlakes_Json,
-      FirePlace_Json,
-      SantaSleigh_Json,
-      RocketLaunches_Json,
-      WebDev_Json,
-      DrawingWoman_Json,
-      ChristmasTree_Json,
-      CatLoader_Json,
-      AnimationSuccessBack_Json,
-      Lights_Json,
-      MerryChristmas_Json,
-      RabbitRunning_Json,
-      BellOfHappiness_Json,
+let webDev_Direction = 1;
+function webDev_LoopComplete() {
+  webDev_Direction = -webDev_Direction;
 
-      BellOfHappiness_playState: false,
-      DayEntered: true,
-      NightEntered: false,
-    }
-  },
-  
-  methods: {
-    bellOfHappiness_OnClick() {
-      this.BellOfHappiness_playState = !this.BellOfHappiness_playState;
-      this.stateChanged();
-    },
+  WebDev.value.pause();
+  WebDev.value.setDirection(webDev_Direction);
+  if (webDev_Direction > 0)
+    WebDev.value.goToAndPlay(0, true);
+  else
+    WebDev.value.goToAndPlay(WebDev.value.getDuration(true), true);
+}
 
-    webDev_LoopComplete() {
-      webDev_Direction = -webDev_Direction;
+let christmasTree_Direction = 1;
+function christmasTree_LoopComplete() {
+  christmasTree_Direction = -christmasTree_Direction;
 
-      this.$refs.WebDev.pause();
-      this.$refs.WebDev.setDirection(webDev_Direction);
-      if (webDev_Direction > 0)
-        this.$refs.WebDev.goToAndPlay(0, true);
-      else
-        this.$refs.WebDev.goToAndPlay(this.$refs.WebDev.getDuration(true), true);
-    },
+  ChristmasTree.value.pause();
+  ChristmasTree.value.setDirection(christmasTree_Direction);
+  if (christmasTree_Direction > 0)
+    ChristmasTree.value.goToAndPlay(0, true);
+  else
+    ChristmasTree.value.goToAndPlay(ChristmasTree.value.getDuration(true), true);
+}
 
-    christmasTree_LoopComplete() {
-      christmasTree_Direction = -christmasTree_Direction;
+let DayEntered = true;
+let NightEntered = false;
 
-      this.$refs.ChristmasTree.pause();
-      this.$refs.ChristmasTree.setDirection(christmasTree_Direction);
-      if (christmasTree_Direction > 0)
-        this.$refs.ChristmasTree.goToAndPlay(0, true);
-      else
-        this.$refs.ChristmasTree.goToAndPlay(this.$refs.ChristmasTree.getDuration(true), true);
-    },
+function night_BeforeEnter() {
+  NightEntered = false;
+  DayEntered = false;
+  stateChanged();
+}
 
-    night_BeforeEnter() {
-      this.NightEntered = false;
-      this.DayEntered = false;
-      this.stateChanged();
-    },
-
-    night_AfterEnter() {
-      this.NightEntered = true;
-      this.stateChanged();
-    },
+function night_AfterEnter() {
+  NightEntered = true;
+  stateChanged();
+}
     
-    night_BeforeLeave() {
-      this.NightEntered = false;
-      this.DayEntered = false;
-      this.stateChanged();
-    },
+function day_BeforeEnter() {
+  NightEntered = false;
+  DayEntered = false;
+  stateChanged();
+}
 
-    night_AfterLeave() {
-      this.DayEntered = true;
-      this.stateChanged();
-    },
+function day_AfterLeave() {
+  DayEntered = true;
+  stateChanged();
+}
 
-    stateChanged() {
-      if (this.BellOfHappiness_playState)
-      {
-        audio.play();
-        this.$refs.BellOfHappiness.play();
-      }
-      else
-      {
-        audio.pause();
-        this.$refs.BellOfHappiness.pause();
-      }
+function stateChanged() {
+  if (BellOfHappiness_playState.value)
+  {
+    Audio.value.play();
+    BellOfHappiness.value.play();
+  }
+  else
+  {
+    Audio.value.pause();
+    BellOfHappiness.value.pause();
+  }
 
-      if (this.BellOfHappiness_playState && this.NightEntered)
-      {
-        this.$refs.HeartFlyPartical.pause();
-        this.$refs.WebDev.pause();
-        this.$refs.DrawingWoman.pause();
-        this.$refs.CatLoader.pause();
-      }
-      else
-      {
-        this.$refs.HeartFlyPartical.play();
-        this.$refs.WebDev.play();
-        this.$refs.DrawingWoman.play();
-        this.$refs.CatLoader.play();
-      }
+  if (BellOfHappiness_playState.value && NightEntered)
+  {
+    HeartFlyPartical.value.pause();
+    WebDev.value.pause();
+    DrawingWoman.value.pause();
+    CatLoader.value.pause();
+  }
+  else
+  {
+    HeartFlyPartical.value.play();
+    WebDev.value.play();
+    DrawingWoman.value.play();
+    CatLoader.value.play();
+  }
 
-      if (!this.BellOfHappiness_playState && this.DayEntered)
-      {
-        this.$refs.SnowFlakes.pause();
-        this.$refs.RocketLaunches.pause();
-        this.$refs.AnimationSuccessBack.pause();
-        this.$refs.MerryChristmas.pause();
-      }
-      else
-      {
-        this.$refs.SnowFlakes.play();
-        this.$refs.RocketLaunches.play();
-        this.$refs.AnimationSuccessBack.play();
-        this.$refs.MerryChristmas.play();
-      }
-    },
-  },
-
-  mounted () {
-    audio = new Audio(DiscoCrash_NewYear_mp3);
-    audio.loop = true;
-
-    this.stateChanged();
-  },
-
-  unmounted () {
-    audio.pause();
-    audio = null;
+  if (!BellOfHappiness_playState.value && DayEntered)
+  {
+    SnowFlakes.value.pause();
+    RocketLaunches.value.pause();
+    AnimationSuccessBack.value.pause();
+    MerryChristmas.value.pause();
+  }
+  else
+  {
+    SnowFlakes.value.play();
+    RocketLaunches.value.play();
+    AnimationSuccessBack.value.play();
+    MerryChristmas.value.play();
   }
 }
 </script>
